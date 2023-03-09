9f31afcc071ec47ae4fbfe1a7b24a70d69d0b388
saif zhioua <85803267+sai4w@users.noreply.github.com>
2023-03-09 21:41:18 +0100

    init

---
+++ client/src/Components/Home.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+// MUI imports
+import { Button, Typography, Grid, AppBar, Toolbar } from "@mui/material";
+
+// Components
+import CustomCard from "./CustomCard";
+import Header from "./Header";
+
+// Assets
+import city from "./Assets/city.jpg";
+
+function Home() {
+	const [btnColor, setBtnColor] = useState("error");
+	const navigate = useNavigate();
+
+
+	return (
+		<>
+			 <div style={{ position: "relative" }}>
+				<img src={city} style={{ width: "100%", height: "92vh" }} />
+				<div
+					style={{
+						position: "absolute",
+						zIndex: "100",
+						top: "100px",
+						left: "20px",
+						textAlign: "center",
+					}}
+				>
+					<Typography
+						variant="h1"
+						style={{ color: "white", fontWeight: "bolder" }}
+					>
+						FIND YOUR <span style={{ color: "green" }}>NEXT PROPERTY</span> ON
+						THE LBREP WEBSITE
+					</Typography>
+					<Button
+					onClick={() => navigate("/listings")}
+						variant="contained"
+						style={{
+							fontSize: "3.5rem",
+							borderRadius: "15px",
+							backgroundColor: "green",
+							marginTop: "2rem",
+							boxShadow: "3px 3px 3px white",
+						}}
+					>
+						SEE ALL PROPERTIES
+					</Button>
+				</div>
+			</div> 
+			<div
+				style={{
+					backgroundColor: "lightgray",
+					width: "100%",
+					height: "100vh",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					fontSize: "3rem",
+					fontWeight: "bolder",
+				}}
+			>
+				WEBSITE UNDER MAINTENANCE...COMING BACK SOON!
+			</div>
+		</>
+	);
+}
+
+export default Home;
